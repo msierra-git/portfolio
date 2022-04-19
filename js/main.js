31f29a7042edf79c236db44987d4094c16836eda
Michael <sie20003@byui.edu>
2022-04-19 14:42:52 +1000

    working on menu js - part 01

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ const assignLinks = [
       url: "week01/index.html"
    }
 ]
+
+var select = document.getElementById('ol');
+var array = ["html", "css", "js", "jquery", "Angularjs"];
+
+for (var i = 0; i < array.length; i++) {
+  var li = document.createElement("li");
+  var link = document.createElement("a");
+  link.setAttribute('href', '#');
+  li.appendChild(link);
+  var text = document.createTextNode(array[i]);
+  link.appendChild(text);
+  select.insertBefore(li, select.childNodes[i]);
+}
