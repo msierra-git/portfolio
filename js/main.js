0c480abfb0127128c83ccd08b19dbe47bfa40de8
Michael <sie20003@byui.edu>
2022-04-19 18:00:11 +1000

    working on menu - part 03

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
-// const assignLinks = [
-//    {
-//       label: "Week 01 -Notes",
-//       url: "week01/index.html"
-//    }
-// ]
+/*==========================================================
+ *   Course Code:     WDD330 - Web Frontend Development II *
+ *   Description:     Assignment 1 - Creating Portfolio    *
+ *   Student Name:    A. Michael Sierra                    *
+ *   Date:            April 2022                           *
+ ==========================================================*/
 
-var select = document.getElementById('ol');
-var array = ["html", "css", "js", "jquery", "Angularjs"];
 
-for (var i = 0; i < array.length; i++) {
-  var li = document.createElement("li");
-  var link = document.createElement("a");
-  link.setAttribute('href', '#');
-  li.appendChild(link);
-  var text = document.createTextNode(array[i]);
-  link.appendChild(text);
-  select.insertBefore(li, select.childNodes[i]);
+function createMenu() {  
+   var menuArrays = [
+      {
+         label:   "Week 01 Notes",
+         url:     "week01/index.html"
+      },
+      {
+         label:   "Week 02 Notes",
+         url:     "week02/index.html"
+      }
+   ];
+   var menuLinks = document.getElementById('ol');
+
+   for (i = 0; i <= menuArrays.length - 1; i++) {
+      var menuItem = document.createElement('li');     // create li element
+      var itemLink = document.createElement('a');      // create a element
+      var itemText = menuArrays[i].label;              // get label from array to variable
+      var itemUrl =  menuArrays[i].url;                // get url from array to variable
+
+      itemLink.setAttribute('href', itemUrl);          // assign url variable to href in <a> element
+      itemLink.innerText = itemText;                   // assign label to text in <a> element
+      menuItem.appendChild(itemLink);                  // append link <a> to item <li>
+
+      menuLinks.appendChild(menuItem);                 // append item <li> to menu <ol>
+   }
 }
