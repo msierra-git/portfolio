79679f5f25ee425c705007f276273ed70b547102
Michael <sie20003@byui.edu>
2022-05-13 14:27:49 +1000

    changing event from click to touchend

--- week04/team-activity/team.js
+++ week04/team-activity/team.js
@@ -1,7 +1,7 @@
 let currentPlayer = 1;
 let board = document.querySelector("#ticTac");
 
-board.addEventListener('click', (a) => {
+board.addEventListener('touchend', (a) => {
    if (a.target.innerHTML.length > 0) {
       // stretch challenge -- add rule for user to choose empty slot
       alert("Choose another field!");
