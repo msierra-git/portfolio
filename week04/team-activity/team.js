12b5b2ab661bbd20ce8502d67a500a1b1fb05c3d
Michael <sie20003@byui.edu>
2022-05-18 01:21:08 +1000

    adding both touchend and click listener

--- week04/team-activity/team.js
+++ week04/team-activity/team.js
@@ -1,14 +1,30 @@
 let currentPlayer = 1;
 let board = document.querySelector("#ticTac");
 
-board.addEventListener('touchend', (a) => {
-   if (a.target.innerHTML.length > 0) {
+// version #1 for adding event listener
+// board.addEventListener('touchend', (a) => {
+//    if (a.target.innerHTML.length > 0) {
+//       // stretch challenge -- add rule for user to choose empty slot
+//       alert("Choose another field!");
+//    } else {
+//       a.target.innerHTML = playerEntry()
+//    }
+// }, false);
+
+
+board.addEventListener('touchend', handler, false);
+board.addEventListener('click', handler, false);
+
+
+function handler(event) {
+   if (event.target.innerHTML.length > 0) {
       // stretch challenge -- add rule for user to choose empty slot
       alert("Choose another field!");
    } else {
-      a.target.innerHTML = playerEntry()
+      event.target.innerHTML = playerEntry()
    }
-}, false);
+}
+
 
 function playerEntry() {
    if (currentPlayer === 1) {
