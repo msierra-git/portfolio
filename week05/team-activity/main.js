ddd77afe7c663a70d3b4097b5de41786d08d1d2e
Michael <sie20003@byui.edu>
2022-05-20 15:44:03 +1000

    modified, refactor codes, format page for team

--- week05/team-activity/main.js
+++ week05/team-activity/main.js
@@ -1,9 +1,11 @@
 import * as dat from "./data.js" 
 import * as utls from "./utils.js";
 
+// added to main.js by M.Sierra
 let myData = dat.getAllHikes();
 
-//on load grab the array and insert it into the page
+// modified and added to main.js by M.Sierra
+// on load grab the array and insert it into the page
 window.addEventListener("load", () => {
    utls.showHikeList(myData);
 });
--- week05/team-activity/utils.js
+++ week05/team-activity/utils.js
@@ -7,39 +7,21 @@ export function showHikeList(hikes) {
    renderHikeList(hikes, hikeListElement);
 }
 
-// function renderHikeList(hikes, parent) {
-//     hikes.forEach(hike => {
-//         parent.appendChild(renderOneHike(hike));
-//     });
-// }
-
+// modified by M.Sierra
 export function renderHikeList(hikes, parent) {
    hikes.forEach(hike => {
       let liItem = renderOneHike(hike);
       parent.appendChild(liItem);
-      liItem.addEventListener('click', function () {
-         let resetButton = document.createElement('button');
-         resetButton.setAttribute('id', "reset");
-         resetButton.innerHTML = "View Full List";
-         
-         resetButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            showHikeList(hikes);
-         }, false);
-
-         parent.innerHTML = "";
-         parent.appendChild(liItem);
-         parent.appendChild(resetButton);
-      }, false);
+      bindHikeListener(parent, liItem, hike, hikes);
    });
 }
 
-export function renderOneHike(hike) {
+function renderOneHike(hike) {
    const item = document.createElement("li");
 
    item.innerHTML = ` <h2>${hike.name}</h2>
           <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-          <div class="description">
+          <div id="details">
                   <div>
                       <h3>Distance</h3>
                       <p>${hike.distance}</p>
@@ -49,7 +31,49 @@ export function renderOneHike(hike) {
                       <p>${hike.difficulty}</p>
                   </div>
           </div>`;
-   item.setAttribute('id', hike.name);
 
    return item;
+}
+
+// added by M.Sierra
+function showHikeDetails(hike, item) {
+   let divDesc = document.createElement("div");
+   divDesc.setAttribute("id", "hikeDetails");
+   divDesc.innerHTML = `
+                  <div>
+                     <h3>Description</h3>
+                     <p>${hike.description}</p>
+                  </div>
+                  <div>
+                     <h3>Directions</h3>
+                     <p>${hike.directions}</p>
+                  </div>
+   `;
+   if (item.querySelector("#hikeDetails") === null) {
+      item.appendChild(divDesc);
+   }
+   return item;
+}
+
+// added by M.Sierra
+function bindHikeListener(parent, item, hike, hikes) {
+   item.addEventListener("click", function () {
+      parent.innerHTML = "";
+      parent.appendChild(showHikeDetails(hike, item));
+      // item.appendChild
+      bindResetListener(parent, hikes);
+   }, false);
+}
+
+// added by M.Sierra
+function bindResetListener(parent, hikes) {
+   let resetButton = document.createElement("button");
+   resetButton.innerHTML = "View Full List";
+
+   resetButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      showHikeList(hikes);
+   }, false);
+
+   parent.appendChild(resetButton);
 }
